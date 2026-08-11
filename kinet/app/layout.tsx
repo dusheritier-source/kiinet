23bfaca787e4704527bed1c3772387d4e0fbab24
ganzaserge3-cmyk <ganzaserge3-cmyk@users.noreply.github.com>
2026-08-11 12:45:38 +0200

    Add Google AdSense script

--- kinet/app/layout.tsx
+++ kinet/app/layout.tsx
@@ -54,6 +54,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6169342782691776"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={`${inter.className} bg-background text-foreground`}>
         <ReactQueryProvider>
           <AuthProvider>
